1f5c6221a98fdb9fc01c0906f791455f8ff2583f
TKbergamot <berg4m0t@gmail.com>
2020-03-01 17:20:15 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'baad1eec4a46938d22a00623121d6b69becc90d79049385a9ce9fe6ed58b03bc62bf7ed64187f75ecbdfd49294f726424dafc0c98d9085346af7faffd0a9d477'
